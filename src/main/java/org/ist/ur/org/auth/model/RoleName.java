60bfae674d6136dc01c7099cbf20792ac7de1bea
Jens <jens@ist-ur.org>
2019-04-06 00:20:39 +0200

    intermediate commit

---
+++ src/main/java/org/ist/ur/org/auth/model/RoleName.java
@@ -0,0 +1,6 @@
+package org.ist.ur.org.auth.model;
+
+public enum RoleName {
+  ROLE_USER,
+  ROLE_ADMIN
+}
---
+++ src/main/java/org/ist/ur/org/auth/model/User.java
@@ -0,0 +1,86 @@
+package org.ist.ur.org.auth.model;
+
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "id")
+  private Long id;
+
+  @Column(name = "username")
+  @NotBlank
+  private String username;
+
+  @Column(name = "email")
+  @NaturalId
+  @Email
+  private String email;
+
+  @Column(name = "password")
+  @NotNull
+  private String password;
+
+  @ManyToMany(fetch = FetchType.LAZY)
+  @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+  private Set<Role> roles = new HashSet<>();
+
+  protected User() {
+  }
+
+  public User(String username, String email, String password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public Set<Role> getRoles() {
+    return roles;
+  }
+
+  public void setRoles(Set<Role> roles) {
+    this.roles = roles;
+  }
+}
