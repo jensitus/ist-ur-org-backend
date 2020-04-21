c1a9e4fcd5a4ceb19e97e81112371947ed8bb515
Jens <jens@ist-ur.org>
2020-04-21 16:15:58 +0200

    Message and EmailStuff

--- src/main/java/org/ist/ur/org/auth/serviceimpl/UserServiceImpl.java
+++ src/main/java/org/ist/ur/org/auth/serviceimpl/UserServiceImpl.java
@@ -7,7 +7,7 @@ import org.ist.ur.org.auth.repository.PasswordResetTokenRepo;
 import org.ist.ur.org.auth.repository.UserRepo;
 import org.ist.ur.org.auth.services.UserService;
 import org.ist.ur.org.auth.security.JwtProvider;
-import org.ist.ur.org.common.RestPoints;
+import org.ist.ur.org.common.util.EmailStuff;
 import org.ist.ur.org.mailer.service.UrOrgMailer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -41,8 +41,8 @@ public class UserServiceImpl implements UserService {
     LocalDateTime localDateTime = LocalDateTime.now();
     PasswordResetToken passwordResetToken = new PasswordResetToken(user, token, localDateTime);
     passwordResetTokenRepo.save(passwordResetToken);
-    String url = RestPoints.BACKENDURL.getValue() + "/reset-password/" + base64token + "/edit?email=" + user.getEmail();
-    String subject = "[ist-ur.org] reset instructions";
+    String url = EmailStuff.DOMAIN_FOR_URL + "/reset-password/" + base64token + "/edit?email=" + user.getEmail();
+    String subject = EmailStuff.SUBJECT_PREFIX + " reset instructions";
     String text = "click the fucking link below";
     urOrgMailer.getTheMailDetails(user.getEmail(), subject, text, user.getUsername(), url);
   }
---
+++ src/main/java/org/ist/ur/org/common/util/EmailStuff.java
@@ -0,0 +1,9 @@
+package org.ist.ur.org.common.util;
+
+public class EmailStuff {
+
+  public static final String DOMAIN_FOR_URL = "https://ist-ur.org";
+
+  public static final String SUBJECT_PREFIX = "[ist-ur.org]";
+
+}
